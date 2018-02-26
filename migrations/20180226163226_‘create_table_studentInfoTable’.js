6348e4116a4fe3340babad6d808a0f7601637902
Davia DeNisco <davia622@gmail.com>
2018-02-26 17:16:14 -0500

    create schema and put seeds folder in project

--- migrations/20180226163226_‘create_table_studentInfoTable’.js
+++ migrations/20180226163226_‘create_table_studentInfoTable’.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("studentInfoTable", (table) => {
+    // id
+    table.increments();
+    // full name
+    table.string("fullName");
+    // github
+    table.string("gitHub");
+    // linkedin
+    table.string("linkedIn");
+    // email address
+    table.string('email').notNull();
+    // personal/professional website
+    table.string('professionalSite');
+    // linke to deployed capstone project
+    table.string('capstoneLink');
+    // blurb describing the capstone project
+    table.string('capstoneDesc');
+    // timestamps
+    table.timestamps(true, true);
+  });
 };
-
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("studentInfoTable");
 };
